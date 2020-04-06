d884a2d02b625ae551161f847b132f8410862447
shivakumarbetel <shivakumar.betel@gmail.com>
2020-04-06 19:43:24 +0530

    deleting unused project

--- playo-webapp/src/main/java/com/ss/playo/webapp/run/PlayoserviceApplication.java
+++ playo-webapp/src/main/java/com/ss/playo/webapp/run/PlayoserviceApplication.java
@@ -1,18 +1,23 @@
 package com.ss.playo.webapp;
 
 import com.ss.playo.common.web.RestResponseEntityExceptionHandler;
+import com.ss.userregistration.UserRegistration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:mysql-properties.properties")
-@Import(RestResponseEntityExceptionHandler.class)
+@Import({RestResponseEntityExceptionHandler.class, UserRegistration.class})
+
 public class PlayoserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlayoserviceApplication.class, args);
+		Class<?>[] bootClasses = new Class[]{PlayoserviceApplication.class, UserRegistration.class};
+
+		SpringApplication.run(bootClasses, args);
 	}
 
 }
